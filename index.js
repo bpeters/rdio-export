5f69f4681b332abe379ca7f2aab330f4698e55c5
bpeters <brennen.peters@gmail.com>
2015-11-26 10:46:49 -0600

    Initialize

---
+++ index.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var app = express();
+
+var rdioConfig = {
+  clientId: '6gi5vuqh2jamheu5xds4uypfqy',
+  clientSecret: '0T7mJSTeHnBqq4IlmPcznw'
+}
+
+var Rdio = require('rdio')({
+  rdio: rdioConfig
+});
+
+var rdio = new Rdio(/* tokens, config*/);
+
+app.get('/', function (req, res) {
+  res.send('<a href="/auth">Login here</a>');
+});
+
+app.get('/auth', function (req, res) {
+  if (!req.query.code) {
+    return res.redirect('https://www.rdio.com/oauth2/authorize?response_type=code&client_id='+rdioConfig.clientId+'&redirect_uri=http://localhost:3000/auth');
+  }
+
+  rdio.getAccessToken({
+    code: req.query.code,
+    redirect: 'http://localhost:3000/auth'
+  }, function(err) {
+    if (err) {
+      return res.send(err);
+    }
+
+    res.redirect('/user');
+  });
+});
+
+app.get('/user', function (req, res) {
+  rdio.request({
+    method: 'currentUser'
+  }, function(err, response) {
+    if (err) {
+      return res.send(err);
+    }
+    res.send('<a href="/favorites">Favorites</a></br><a href="/playlists">Playlists</a>');
+  });
+});
+
+app.get('/favorites', function (req, res) {
+  rdio.request({
+    method: 'getFavorites',
+    start: 0,
+    count: 100000,
+  }, function(err, response) {
+    if (err) {
+      return res.send(err);
+    }
+
+    res.send(response);
+  });
+});
+
+app.get('/playlists', function (req, res) {
+  rdio.request({
+    method: 'getPlaylists',
+    extras: ['tracks']
+  }, function(err, response) {
+    if (err) {
+      return res.send(err);
+    }
+
+    res.send(response);
+  });
+});
+
+app.get('/lastPlayed', function (req, res) {
+  rdio.getClientToken(function(err) {
+    if (err) {
+      return res.send(err);
+    }
+
+    rdio.request({
+      method: 'findUser',
+      vanityName: 'BrennenPeters',
+      extras: 'lastSongPlayed'
+    }, false, function(err, response) {
+      if (err) {
+        return res.send(err);
+      }
+
+      res.send(response);
+    });
+  });
+});
+
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+
+  console.log('Example app listening at http://localhost:%s', port);
+});
